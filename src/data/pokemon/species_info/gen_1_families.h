9ddcf1ae39703ee9becce5674e31d7d83198a02c
babytrunks <ericchuang98@gmail.com>
2026-02-27 13:30:48 -0700

    Start on support for Hard Mode

--- src/data/pokemon/species_info/gen_1_families.h
+++ src/data/pokemon/species_info/gen_1_families.h
@@ -14779,8 +14779,7 @@ const struct SpeciesInfo gSpeciesInfoGen1[] =
         .levelUpLearnset = sHappinyLevelUpLearnset,
         .teachableLearnset = sHappinyTeachableLearnset,
         .eggMoveLearnset = sHappinyEggMoveLearnset,
-        .evolutions = EVOLUTION({EVO_LEVEL, 0, SPECIES_CHANSEY, CONDITIONS({IF_NOT_TIME, TIME_NIGHT},{IF_HOLD_ITEM, ITEM_OVAL_STONE})},
-                                {EVO_ITEM, ITEM_OVAL_STONE, SPECIES_CHANSEY, CONDITIONS({IF_NOT_TIME, TIME_NIGHT})}),
+        .evolutions = EVOLUTION({EVO_LEVEL, 35, SPECIES_CHANSEY}),
     },
 #endif //P_GEN_4_CROSS_EVOS
 
@@ -14852,7 +14851,7 @@ const struct SpeciesInfo gSpeciesInfoGen1[] =
         .teachableLearnset = sChanseyTeachableLearnset,
         .eggMoveLearnset = sChanseyEggMoveLearnset,
     #if P_GEN_2_CROSS_EVOS
-        .evolutions = EVOLUTION({EVO_LEVEL, 40, SPECIES_BLISSEY}),
+        .evolutions = EVOLUTION({EVO_LEVEL, 45, SPECIES_BLISSEY}),
     #endif
     },
 
