7862497bdb1c32c2931c3504a0907cdfa2a2bd32
babytrunks <ericchuang98@gmail.com>
2026-04-22 15:50:47 -0700

    Fix Red Biking

--- src/data/outfit_tables.h
+++ src/data/outfit_tables.h
@@ -88,7 +88,7 @@ const struct Outfit gOutfits[OUTFIT_COUNT] =
         .avatarGfxIds = {
            [MALE] = {
                [PLAYER_AVATAR_STATE_NORMAL] =     OBJ_EVENT_GFX_RED_NORMAL,
-               [PLAYER_AVATAR_STATE_BIKE] =       OBJ_EVENT_GFX_RIVAL_MAY_ACRO_BIKE,
+               [PLAYER_AVATAR_STATE_BIKE] =       OBJ_EVENT_GFX_RED_BIKE,
                [PLAYER_AVATAR_STATE_SURFING] =    OBJ_EVENT_GFX_RED_SURF,
                [PLAYER_AVATAR_STATE_UNDERWATER] = OBJ_EVENT_GFX_BRENDAN_UNDERWATER
            },
